8a633351cb6af807d70aa97e1a555b634ac5063a
Yang Su <suyang1+services@gmail.com>
2012-06-07 22:48:10 -0700

    added appstore scraping script and the downloaded data

--- data/appstore.sh
+++ data/appstore.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 FILE="itunes.cat"
 OUTFILE="appstore.json"
-BASE="http://iicns.com"
-CATEGORY="categories/"
-ECATEGORY=$(echo $CATEGORY | sed -nE "s/\//\\\\\//p")
-CATEGORYFILE="categories.json"
+OUTIMAGES="appstore.complete.json"
+OUTDIR="appstore/"
 
 function toJson() {
     sed -nE "/\<div\ id=\"selectedcontent\"/,/\<\/ul\>/{
@@ -22,63 +20,43 @@ function scrape() {
         cat=$(echo $line | sed -nE "s/[^,]+,(.+)/\\1/p")
         echo "\""$cat"\": ["
         linkToJson $link
-        echo "]"
+        echo "],"
     done
     echo "}"
 }
 
-# function download() {
-#   cat $FILE | sed -nE 's/.+image\"\:\"([^\"]+)\".+/\1/p' | awk '
-#   BEGIN {
-#     d[512];
-#     d[256];
-#     d[144];
-#     d[114];
-#     d[72];
-#     d[57];
-#   }
-#   {
-#     for (x in d) {
-#       sub(/s[0-9]{2,3}/, "s"x)
-#       system("wget -q -P "outdir" "$1" > /dev/null 2>&1");
-#       print $1
-#     }
-#   }' outdir=$OUTDIR
-# }
+function extractImgLink() {
+    cat $OUTFILE | awk '
+    function linkToJson(link) {
+        command="echo \\\"image\\\":\\\"$(curl -s "link" | grep \"width=\\\"175\\\"\" | sed -nE \"s/.+src=\\\"([^\\\"]+)\\\".+/\\\\1/p\")\\\","
+        system(command)
+    }
+    /link/ {
+        linkToJson($2)
+    }
+    {
+        print $0
+    }
+    '
+}
 
-# function downloadCategories() {
-#     #| sed -nE s/.+href=\"([^\"]+)\".+alt=\"([^\"]+)\".+src=\"([^\"]+)\".+/\{\"name\": \"\\2\",\"link\":\"\\1\",\"image\":\"\\3\"},/p
-#   for item in {"/?category=Business|Business","/?category=Photo+%26+Video|Photo &amp; Video","/?category=Reference|Reference","/?category=Lifestyle|Lifestyle","/?category=Music|Music","/?category=Productivity|Productivity","/?category=Utilities|Utilities","/?category=Social+Networking|Social Networking","/?category=Games|Games","/?category=Entertainment|Entertainment","/?category=Health+%26+Fitness|Health &amp; Fitness","/?category=Travel|Travel","/?category=Education|Education","/?category=Navigation|Navigation","/?category=Sports|Sports","/?category=News|News","/?category=Finance|Finance","/?category=Weather|Weather","/?category=Book|Book","/?category=Catalogs|Catalogs","/?category=Medical|Medical","/?tag=Colorful|Colorful","/?tag=Fabric|Fabric","/?tag=Leather|Leather","/?tag=Metal|Metal","/?tag=Minimalistic|Minimalistic","/?tag=Pixel+art|Pixel art","/?tag=Wooden|Wooden","/?perspective=2D|2D","/?perspective=3D|3D"}; do
-#     echo $item | awk -F '|' '
-#     function linkToJson(link, file, page) {
-#       command="curl -s \""link"&page="page"\" | sed -nE \"/\\\<ul id=\\\"icons\\\"/,/\\\<\\\/ul\>/p\" >> \""category""file".txt\""
-#       print command
-#       system(command)
-#     }
-#     {
-#       for (i = 1; i <= 10; i++) {
-#         print $1"&page="i
-#         print linkToJson(base$1, $2, i)
-#       }
-#     }' base=$BASE category=$CATEGORY
-#   done
-# }
+function download() {
+    cat $OUTIMAGES | awk '
+    function dl(link) {
+        command="wget -q -P "outdir" "link" > /dev/null 2>&1"
+        system(command)
+    }
+    /image/ {
+        sub(/175x175/, "100x100")
+        sub(/,/, "")
+        dl($2)
+    }
+    ' outdir=$OUTDIR
+}
 
-# function extractCategories() {
-#   echo "{"
-#   for file in $CATEGORY*; do
-#     cate=$(echo $file | sed -nE "s/$ECATEGORY(.+)\.txt/\\1/p")
-#     echo "\"$cate\" : ["
-#     cat "$file" | toJson
-#     echo "],"
-#   done
-#   echo "}"
-# }
-
-scrape > $OUTFILE
-# download
-# downloadCategories
-# extractCategories > $CATEGORYFILE
+# scrape > $OUTFILE
+# extractImgLink > $OUTIMAGES
+download
 
 
 
